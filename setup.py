df0a5573da5292dcc739f66da03d527a595818de
hilaBGU <114908736+hilaBGU@users.noreply.github.com>
2023-04-23 12:51:34 +0300

    trying to fix error in colab run - add MANIFEST.in file

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ long_description=\
 
 setup(name='up_cpor',
       version=up_cpor.__version__,
-      include_package_data=True,
       description='up_cpor',
       author='BGU CPOR Development Team',
       author_email='shanigu@bgu.ac.il',
       url='',
-      packages= find_packages(),
+      packages=['CPORLib','up_cpor'],
       install_requires=['pythonnet==3.0.0'],
       python_requires='>=3.7',
-      package_data={"CPORLib": ["**/*.dll", "**/*.exe"]},
+      # package_data={"CPORLib": ["CPORLib/obj/Debug/netstandard2.0/CPORLib.dll"]},
+      include_package_data=True,
       license='APACHE'
 )
